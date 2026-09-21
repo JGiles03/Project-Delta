9553be5c403ed4010c8ace8139adb5c3d904e127
JGiles03 <josephsgiles@gmail.com>
2026-09-21 14:36:22 +0100

    basic homepage

--- project-delta-frontend/src/Pages/HomePage/index.tsx
+++ project-delta-frontend/src/Pages/HomePage/index.tsx
@@ -1,5 +1,12 @@
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>Welcome to <em>insert project name here</em>!</h1>
+      <p>This is a project to give solo parents peace of mind when they are planning any activites involving a 3rd party
+        <br/> Log in or sign in below to access the map and planner
+      </p>
+      <a href="/login"><button>Log in</button></a>
+      <a href="/signup"><button>Sign up</button></a>
+    </div>
   )
 }

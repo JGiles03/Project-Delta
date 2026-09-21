3deccd02748c015108eff16586da866535cc40e2
JGiles03 <josephsgiles@gmail.com>
2026-09-21 16:30:20 +0100

    stuff

--- project-delta-frontend/src/Components/Header/index.tsx
+++ project-delta-frontend/src/Components/Header/index.tsx
@@ -6,8 +6,8 @@ export default function Header() {
     <header>
         <nav>
             <NavLink className="nav-links" to='/home'>Map</NavLink>
-            <NavLink className="nav-links" to='/list'>List</NavLink>
-            <NavLink className="nav-links" to='/account'>Account</NavLink>
+            <NavLink className="nav-links" to='/home/list'>List</NavLink>
+            <NavLink className="nav-links" to='/home/account'>Account</NavLink>
         </nav>
     </header>
     <Outlet/>

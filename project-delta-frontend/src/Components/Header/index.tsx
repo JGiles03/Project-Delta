f83724eb6a1dca0e9cee77bb12c3e194a873f96d
jaydendavis746-debug <jaydendavis746@gmail.com>
2026-09-23 12:27:49 +0100

    changed teh nav to use icons

--- project-delta-frontend/src/Components/Header/index.tsx
+++ project-delta-frontend/src/Components/Header/index.tsx
@@ -1,6 +1,8 @@
 import { NavLink, Outlet } from 'react-router-dom'
 import './index.css'
 import home from '../../assets/home.png'
+import list from '../../assets/list.png'
+import account from '../../assets/account.png'
 
 export default function Header() {
   return (
@@ -10,9 +12,9 @@ export default function Header() {
       </div>
 
       <nav className="bottom-nav">
-        <NavLink className="nav-links" to='/'><img src={home} alt="" /></NavLink>
-        <NavLink className="nav-links" to='/list'>List</NavLink>
-        <NavLink className="nav-links" to='/account'>Account</NavLink>
+        <NavLink className="nav-links" to='/'><img src={home} alt="homepage" /></NavLink>
+        <NavLink className="nav-links list-link" to='/list'><img src={list} alt="listpage" /></NavLink>
+        <NavLink className="nav-links" to='/account'><img src={account} alt="accountpage" /></NavLink>
       </nav>
     </div>
   )
